01d5e8b5290c49e177c2783946b95736c123f3ad
piotr <piotrekp94@gmail.com>
2019-12-28 16:22:23 +0100

    yep

---
+++ Assets/Scripts/ClickableObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    PrintName(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+}
