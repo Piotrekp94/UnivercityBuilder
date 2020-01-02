c98f0cd7cb9e9d5e4143949765ed6a3fae471bd4
piotr <piotrekp94@gmail.com>
2020-01-03 00:07:46 +0100

    :)

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Startup : MonoBehaviour
+{
+    // called first
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called111");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
