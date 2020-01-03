1beebee7c5ff0358ff23be9570f91bd5c7cc3525
piotr <piotrekp94@gmail.com>
2020-01-03 01:03:11 +0100

    dzialajace UI

---
+++ Assets/Scripts/UI/updateMoneyValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateMoneyValue : MonoBehaviour
+{
+    Text moneyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = PersistentUIManager.Instance.Money.ToString();
+    }
+}
---
+++ Assets/Scripts/UI/updateStudentsValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateStudentsValue : MonoBehaviour
+{
+    Text studentsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        studentsText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        studentsText.text = PersistentUIManager.Instance.Students.ToString();
+    }
+}
