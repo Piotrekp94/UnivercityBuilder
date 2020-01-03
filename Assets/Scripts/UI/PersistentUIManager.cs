7de042326489bc6a43c7ea4edc483259a50608b9
piotr <piotrekp94@gmail.com>
2020-01-03 01:03:04 +0100

    dzialajace UI

--- Assets/Scripts/UI/PersistentUIManager.cs
+++ Assets/Scripts/UI/PersistentUIManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PersistentUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PersistentUIManager Instance { get; private set; }
+
+    public int Students;
+    public int Money;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
