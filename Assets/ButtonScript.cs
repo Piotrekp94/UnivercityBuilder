1f14dfce5680e46acba3278de73f6148aadcc2d0
piotr <piotrekp94@gmail.com>
2020-01-24 14:11:09 +0100

    10h to go

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Button button;
+    public Sprite on;
+    public Sprite off;
+    public GameObject prefab;
+    
+
+    private SizeScript sizeScript;
+    private ManagerStatistic manager;
+    public StatsPanelScript statsPanel;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.image.sprite = off;
+        sizeScript = prefab.GetComponent<SizeScript>();
+        manager = ManagerStatistic.Instance;
+    }
+
+    void Update()
+    {
+        if (manager.canBuy(sizeScript.prize))
+        {
+            button.image.sprite = on;
+            if (button.GetComponent<Button>().IsInteractable() == false)
+            {
+                button.GetComponent<Button>().interactable = true;
+            }
+        }
+        else 
+        {
+            button.image.sprite = off;
+            if (button.GetComponent<Button>().IsInteractable() == true)
+            {
+                button.GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        statsPanel.toggle();
+        statsPanel.setString("Cost: " + sizeScript.prize.ToString() + "\n" + "Maintenance: " + sizeScript.maintenanceCost.ToString());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        statsPanel.toggle();
+    }
+}
---
+++ Assets/StatsPanelScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StatsPanelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI script;
+    public bool isToggled = false;
+    MeshRenderer panel;
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void toggle()
+    {
+        isToggled = !isToggled;
+        this.gameObject.SetActive(isToggled);
+
+    }
+    public void setString(string message)
+    {
+        script.SetText(message);
+    }
+}
